44b51046b080aa0c7c3f70b63faf9e357f014b62
bchapol <202300085@upqroo.edu.mx>
2025-02-18 19:52:08 -0500

    nuevo index

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-/* Prueba */
+const express = require ("express");
+const app = express();
 
-/*Veamos si funciona mi prueba*/
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+/* ROUTES */
+app.use(require('./routes/roles'));
+app.use(require('./routes/users'));
+app.use(require('./routes/employees'));
+
+const PORT = process.env.PORT;
+app.listen (PORT, () => {
+    console.log('El servidor escucha en el puerto: ' + PORT);
+});
+
+module.exports = app;
